42ff6585d915c22225936822c1584aa364a1bb8b
Logical Contradiction <chrisgiaramita@yahoo.com>
2022-06-04 13:29:09 -0400

    added link to other playlist and placeholder for this playlist.

--- scripts/main.js
+++ scripts/main.js
@@ -13,7 +13,13 @@ export default {
 	},
 	template:`
 	<p>This is a fan-made website dedicated to keeping track of the songs in Lythero's stream music playlist.</p>
-	<p></p>
+	<br>
+	<p> 
+		<a href="https://www.youtube.com/" target="_blank">YouTube playlist that contains most of the songs in this list.</a>
+	</p>
+	<p>
+		<a href="https://www.youtube.com/playlist?list=PLQFIg1qsgNezAe6-3NgtfTRZlaQXo46LX" target="_blank">Another YouTube playlist for Lythero music</a> (this webpage and list are separate from this playlist).
+	</p>
 
 	<DataList :trackList="trackData"></DataList>
 	`,
