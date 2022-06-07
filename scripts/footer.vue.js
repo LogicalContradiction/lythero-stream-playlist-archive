65344e656f1663ae085ef2d50ca25881421e65a2
Logical Contradiction <chrisgiaramita@yahoo.com>
2022-06-06 23:56:59 -0400

    added more links. Need to put them in the right place now.

--- scripts/footer.vue.js
+++ scripts/footer.vue.js
@@ -1,9 +1,11 @@
 export default {
 	name: 'Footer',
 	template: `
-	<a href="https://www.youtube.com/c/Lythero" target="_blank" class="footerRow youtubeFooterRow"><img src="./images/youtube_icon_black.svg" height="26" class="footerIcon">Lythero's Youtube Channel</a><br>
-	<a href="https://twitter.com/Lythero" target="_blank" class="footerRow twitterFooterRow"><img src="./images/twitter_icon_black.svg" height="22" class="footerIcon">Lythero's Twitter</a><br>
-	<a href="https://www.twitch.tv/Lythero" target="_blank" class="footerRow twitchFooterRow"><img src="./images/twitch_icon_black.svg" height="31" class="footerIcon">Lythero's Twitch</a><br>
+	<a href="https://www.twitch.tv/Lythero" target="_blank" class="footerRow twitchFooterRow"><img src="./images/twitch_icon_black.svg" height="31" class="footerIcon">Twitch</a><br>
+	<a href="https://www.youtube.com/c/Lythero" target="_blank" class="footerRow youtubeFooterRow"><img src="./images/youtube_icon_black.svg" height="26" class="footerIcon">Youtube</a><br>
+	<a href="https://www.youtube.com/c/LytheroStreams" target="_blank" class="footerRow youtubeFooterRow"><img src="./images/youtube_icon_black.svg" height="26" class="footerIcon">Stream VOD Channel</a><br>
+	<a href="https://www.youtube.com/channel/UCROuhoNylPaQB7vYQYSGTqA" target="_blank" class="footerRow youtubeFooterRow"><img src="./images/youtube_icon_black.svg" height="26" class="footerIcon">Out of Context Channel</a><br>
+	<a href="https://twitter.com/Lythero" target="_blank" class="footerRow twitterFooterRow"><img src="./images/twitter_icon_black.svg" height="22" class="footerIcon">Twitter</a><br>
 
 	`,
 	data(){
