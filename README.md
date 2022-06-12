f3eb80abaccebc10f22794d6e20746ca8dbf6f80
Logical Contradiction <chrisgiaramita@yahoo.com>
2022-06-11 20:05:02 -0400

    Added readme and images to use for dark theme.

---
+++ README.md
@@ -0,0 +1,32 @@
+## The Lythero Stream Music Playlist Archive
+
+A tool to keep track of songs in Lythero's stream playlist. Provides simple filter functions to allow users to find a particular song.  
+See it here: 
+
+Follow Lythero here:
+* [
+    <picture>
+        <source media="(prefers-color-scheme: dark)" srcset="./images/twitch_icon_white.svg">
+        <img src="./images/twitch_icon_black.svg" height="30" alt="twitch icon" align="center">
+    </picture> Twitch](https://www.twitch.tv/Lythero)
+* [
+    <picture>
+        <source media="(prefers-color-scheme: dark)" srcset="./images/youtube_icon_white.svg">
+        <img src="./images/youtube_icon_black.svg" alt="youtube icon" height="26" align="center">
+    </picture> YouTube Channel](https://www.youtube.com/c/Lythero)
+* [
+    <picture>
+        <source media="(prefers-color-scheme: dark)" srcset="./images/youtube_icon_white.svg">
+        <img src="./images/youtube_icon_black.svg" alt="youtube icon" height="26" align="center">
+    </picture> Stream VOD Channel](https://www.youtube.com/c/LytheroStreams)
+* [
+    <picture>
+        <source media="(prefers-color-scheme: dark)" srcset="./images/youtube_icon_white.svg">
+        <img src="./images/youtube_icon_black.svg" alt="youtube icon" height="26" align="center">
+    </picture> Out of Context YouTube Channel](https://www.youtube.com/channel/UCROuhoNylPaQB7vYQYSGTqA)
+* [
+    <picture>
+        <source media="(prefers-color-scheme: dark)" srcset="./images/twitter_icon_white.svg">
+        <img src="./images/twitter_icon_black.svg" alt="twitter icon" height="22" align="center">
+    </picture> Twitter](https://twitter.com/Lythero)
+ 
--- scripts/footer.vue.js
+++ scripts/footer.vue.js
@@ -10,27 +10,27 @@ export default {
 			<div class="leftFooter">
 				<div class="leftFooterText">
 					<div class="footerDataLink">
-						<a href="https://www.twitch.tv/Lythero" target="_blank"><img src="./images/twitch_icon_black.svg" height="30" class="footerIcon">Twitch</a>
+						<a href="https://www.twitch.tv/Lythero" target="_blank"><img src="./images/twitch_icon_black.svg" alt="twitch icon" height="30" class="footerIcon">Twitch</a>
 					</div>
 					<div class="footerDataLink">
-						<a href="https://twitter.com/Lythero" target="_blank"><img src="./images/twitter_icon_black.svg" height="22" class="footerIcon">Twitter</a>
+						<a href="https://twitter.com/Lythero" target="_blank"><img src="./images/twitter_icon_black.svg" alt="twitter icon" height="22" class="footerIcon">Twitter</a>
 					</div>
 				</div>
 			</div>
 			<div class="centerFooter">
 				<div class="centerFooterText">
 					<div class="footerDataLink">
-						<a href="https://www.youtube.com/c/Lythero" target="_blank" class="footerRow youtubeFooterRow"><img src="./images/youtube_icon_black.svg" height="26" class="footerIcon">Youtube Channel</a>
+						<a href="https://www.youtube.com/c/Lythero" target="_blank" class="footerRow youtubeFooterRow"><img src="./images/youtube_icon_black.svg" alt="youtube icon" height="26" class="footerIcon">Youtube Channel</a>
 					</div>
 					<div class="footerDataLink">
-						<a href="https://www.youtube.com/c/LytheroStreams" target="_blank" class="footerRow youtubeFooterRow"><img src="./images/youtube_icon_black.svg" height="26" class="footerIcon">Stream VOD Channel</a>
+						<a href="https://www.youtube.com/c/LytheroStreams" target="_blank" class="footerRow youtubeFooterRow"><img src="./images/youtube_icon_black.svg" alt="youtube icon" height="26" class="footerIcon">Stream VOD Channel</a>
 					</div>
 				</div>
 			</div>
 			<div class="rightFooter">
 				<div class="rightFooterText">
 					<div class="footerDataLink">
-						<a href="https://www.youtube.com/channel/UCROuhoNylPaQB7vYQYSGTqA" target="_blank" class="footerRow youtubeFooterRow"><img src="./images/youtube_icon_black.svg" height="26" class="footerIcon">Out of Context Channel</a>
+						<a href="https://www.youtube.com/channel/UCROuhoNylPaQB7vYQYSGTqA" target="_blank" class="footerRow youtubeFooterRow"><img src="./images/youtube_icon_black.svg" alt="youtube icon" height="26" class="footerIcon">Out of Context Channel</a>
 					</div>
 				</div>
 			</div>
