797acbb8a925e3b0cd9f366825a4a1fb9981ed09
Logical Contradiction <chrisgiaramita@yahoo.com>
2022-06-21 13:04:31 -0400

    Fixed footer links to be the entire highlighted box instead of just the image + text

--- scripts/footer.vue.js
+++ scripts/footer.vue.js
@@ -9,29 +9,39 @@ export default {
 		<div id="footerLinks">
 			<div class="leftFooter">
 				<div class="leftFooterText">
-					<div class="footerDataLink">
-						<a href="https://www.twitch.tv/Lythero" target="_blank"><img src="./images/twitch_icon_black.svg" alt="twitch icon" height="30" class="footerIcon">Twitch</a>
-					</div>
-					<div class="footerDataLink">
-						<a href="https://twitter.com/Lythero" target="_blank"><img src="./images/twitter_icon_black.svg" alt="twitter icon" height="22" class="footerIcon">Twitter</a>
-					</div>
+					<a href="https://www.twitch.tv/Lythero" target="_blank">
+						<div class="footerDataLink">
+							<img src="./images/twitch_icon_black.svg" alt="twitch icon" height="30" class="footerIcon">Twitch
+						</div>
+					</a>
+					<a href="https://twitter.com/Lythero" target="_blank">
+						<div class="footerDataLink">
+							<img src="./images/twitter_icon_black.svg" alt="twitter icon" height="22" class="footerIcon">Twitter
+						</div>
+					</a>
 				</div>
 			</div>
 			<div class="centerFooter">
 				<div class="centerFooterText">
-					<div class="footerDataLink">
-						<a href="https://www.youtube.com/c/Lythero" target="_blank" class="footerRow youtubeFooterRow"><img src="./images/youtube_icon_black.svg" alt="youtube icon" height="26" class="footerIcon">Youtube Channel</a>
-					</div>
-					<div class="footerDataLink">
-						<a href="https://www.youtube.com/c/LytheroStreams" target="_blank" class="footerRow youtubeFooterRow"><img src="./images/youtube_icon_black.svg" alt="youtube icon" height="26" class="footerIcon">Stream VOD Channel</a>
-					</div>
+					<a href="https://www.youtube.com/c/Lythero" target="_blank">
+						<div class="footerDataLink">
+							<img src="./images/youtube_icon_black.svg" alt="youtube icon" height="26" class="footerIcon">Youtube Channel
+						</div>
+					</a>
+					<a href="https://www.youtube.com/c/LytheroStreams" target="_blank">
+						<div class="footerDataLink">
+							<img src="./images/youtube_icon_black.svg" alt="youtube icon" height="26" class="footerIcon">Stream VOD Channel
+						</div>
+					</a>
 				</div>
 			</div>
 			<div class="rightFooter">
 				<div class="rightFooterText">
-					<div class="footerDataLink">
-						<a href="https://www.youtube.com/channel/UCROuhoNylPaQB7vYQYSGTqA" target="_blank" class="footerRow youtubeFooterRow"><img src="./images/youtube_icon_black.svg" alt="youtube icon" height="26" class="footerIcon">Out of Context Channel</a>
-					</div>
+					<a href="https://www.youtube.com/channel/UCROuhoNylPaQB7vYQYSGTqA" target="_blank">
+						<div class="footerDataLink">
+							<img src="./images/youtube_icon_black.svg" alt="youtube icon" height="26" class="footerIcon">Out of Context Channel
+						</div>
+					</a>
 				</div>
 			</div>
 		</div>
